3bf931a674ed214aacbdd7c75937de7a933906d1
badruddin <badruddin.dev@gmail.com>
2020-08-31 17:50:28 +0800

    Updates

---
+++ precache-manifest.021c1726aab96b1ca93995998879c160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c240fc2362e7036a7416652f38c83b5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f98d79f686b0091899e",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/portfolio/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f98d79f686b0091899e",
+    "url": "/portfolio/static/js/main.a16bd518.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
